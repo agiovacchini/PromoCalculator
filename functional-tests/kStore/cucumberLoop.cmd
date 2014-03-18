71674d347e41bdf573bb4cac3158a872b00e5be0
andrea <giovacchini.andrea@konvergence.it>
2014-03-18 22:07:53 +0100

    Nulla

--- functional-tests/kStore/cucumberLoop.cmd
+++ functional-tests/kStore/cucumberLoop.cmd
@@ -1,4 +1,4 @@
 del d:\Listati\Siti\PromoCalculator\PromoCalculator.deploy\ARCHIVES\001\12003\CARTS\*.transaction_in_progress 
 :inizio
-call cucumber TEST_ENV=local ..\servicesTest\features\kStoreMineLinkedItem.feature
+call cucumber TEST_ENV=lab_ravenna_development ..\servicesTest\features\kStoreMineLinkedItem.feature
 goto inizio
