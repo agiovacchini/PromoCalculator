fa8dafa6832d2a5bce26dbdc877f8659d5bfb1d3
Andrea Giovacchini <giovacchinia@gmail.com>
2022-11-03 23:36:04 +0100

    Removed unud files

--- support.old/env.rb
+++
@@ -1,6 +0,0 @@
-require File.dirname(__FILE__) + '/lib/configuration';
-
-SALES_IP = Configuration["salesIp"]
-SALES_PORT = Configuration["salesPort"]
-puts 'salesIp: ' << SALES_IP
-puts 'salesPort: ' << SALES_PORT
